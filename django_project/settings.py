b5ba3f1fedcfee6ed5cdc875b424d748a2783f5f
Museyib Huseynov <museyib.huseynov.dev@gmail.com>
2020-04-03 00:47:05 +0400

    st1

--- django_project/settings.py
+++ django_project/settings.py
@@ -125,9 +125,11 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
+STATICFILES_DIRS = None
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
