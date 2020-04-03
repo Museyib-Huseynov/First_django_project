2ecdb58bbb070da7fb2d7e64dcaf6961f04c8b7e
Museyib Huseynov <museyib.huseynov.dev@gmail.com>
2020-04-03 11:48:29 +0400

    st6

--- django_project/settings.py
+++ django_project/settings.py
@@ -127,7 +127,8 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'static'),
 # )
